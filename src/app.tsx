4dd22fa42456befa59b1e4f06a608b28937afe8b
David G Chaves <davidgchaves@gmail.com>
2022-11-23 18:51:59 +0100

    Added table + fix wrong url

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const membersMock = [
   {
     login: "Nasdan",
     id: 4374977,
-    avatar_url: "https://avatars.githubusercontent.com/u/4374977?v4",
+    avatar_url: "https://avatars.githubusercontent.com/u/4374977?v=4",
   },
   {
     login: "v-borrego",
     id: 43609530,
-    avatar_url: "https://avatars.githubusercontent.com/u/43609530?v4",
+    avatar_url: "https://avatars.githubusercontent.com/u/43609530?v=4",
   },
-]
-
-const renderMember = (member) => (
-  <span key={member.id}>{member.login}</span>
-);
+];
 
 export const App = () => {
   const [members] = React.useState(membersMock);
+
   return (
-    <>
-      {members.map((member) => ( <span key={member.id}>{member.login}</span> ))}
-    </>
+    <div className="user-list-container">
+      <span className="header">Avatar</span>
+      <span className="header">Id</span>
+      <span className="header">Name</span>
+      {members.map((member) => (
+        <React.Fragment key={member.id}>
+          <img src={member.avatar_url} />
+          <span>{member.id}</span>
+          <span>{member.login}</span>
+        </React.Fragment>
+      ))}
+    </div>
   );
-}
+};
