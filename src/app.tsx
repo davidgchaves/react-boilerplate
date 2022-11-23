bb0f1dcc5fdb5179df8d2caa2796fff60d876874
David G Chaves <davidgchaves@gmail.com>
2022-11-23 18:34:02 +0100

    `useState` hook & 1st map 🎉🥁👽

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const membersMock = [
+  {
+    login: "Nasdan",
+    id: 4374977,
+    avatar_url: "https://avatars.githubusercontent.com/u/4374977?v4",
+  },
+  {
+    login: "v-borrego",
+    id: 43609530,
+    avatar_url: "https://avatars.githubusercontent.com/u/43609530?v4",
+  },
+]
+
+const renderMember = (member) => (
+  <span key={member.id}>{member.login}</span>
+);
+
 export const App = () => {
-  return <h1>Hello React</h1>
-};
+  const [members] = React.useState(membersMock);
+  return (
+    <>
+      {members.map((member) => ( <span key={member.id}>{member.login}</span> ))}
+    </>
+  );
+}
